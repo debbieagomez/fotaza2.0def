9b4aefcc5fc0327189e5568cbd84351812bf3868
debbieagomez <deborahgomez71@gmail.com>
2026-06-02 01:28:01 -0300

    cambio a sequelize

--- src/config/db-init.js
+++ src/config/db-init.js
@@ -1,34 +1,18 @@
 requiere ('dotenv').config();
+const sequelize = requiere('./db');
 
-const mysql = requiere('mysql2/promise');
+async function initDatabase
+() {   try {
+        await sequelize.authenticate();
+        console.log('Conexión a la base de datos establecida con éxito.');
 
-async function initDB() {
-    //sin especificar base de datos
-    const conn = await mysql.createConnection({
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-    }); 
-
-    const DB = process.env.DB_NAME || 'fotaza';
-
-    try{
-        //crear base de datos si no existe
-    await conn.query(`CREATE DATABASE IF NOT EXISTS \`${DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci`);
-    await conn.query(`USE \`${DB}\``);
-    console.log(`Base de datos '${DB}' creada o ya existe.`);
-
-    console.log("todo listo");
-
-    } catch (error){
-        console.error('error', error.message);
-    } finally {
-        await conn.end();
-    }   
-
-    
-
+        await sequelize.sync({force: false});
+        console.log('tablas sincronizadas correctamente.');
+    } catch (error) {
+        console.error('error:', error.message);
+    }finally {
+        await sequelize.close();
+}
 }
 
-innitDB();
+initDatabase();
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,14 @@
-const mysql = requiere('mysql2/promise');
+const{sequelize} = require('sequelize');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'fotaza',
-    waitForConnections: true,
-    connectionLimit: 10,
-});
+const sequelize = new sequelize(
+    process.env.DB_NAME || 'fotaza',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || '',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        logging: false,
+        dialect: 'mysql'
+    }
+);
+
+module.exports = sequelize;
